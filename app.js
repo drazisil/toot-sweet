af64d8cbb8dd776f974c1a1f5410a8b60f637263
Drazi Crendraven <190986+drazisil@users.noreply.github.com>
2022-12-23 13:17:40 -0500

    Can validate http signatures

--- app.js
+++ app.js
@@ -8,11 +8,31 @@ const appName = 'toot-sweet'
 const port = 9000
 // === Init
 const debug = createDebug('toot-sweet')
-const log = createLogger({name: appName})
+const log = createLogger({ name: appName })
 const app = express()
 
+// === Methods
+/**
+ * 
+ * @param {string} keyId 
+ * @returns {Promise<string>}
+ */
+async function fetchActorPublicKey(keyId) {
+  const url = new URL(keyId)
+  return fetch(new URL(keyId))
+    .then(res => {
+      return res.json()
+    }).then(json => {
+      return json.publicKey.publicKeyPem
+    })
+    .catch(err => {
+      log.error(err)
+      throw new Error(`Unable to fetch actor key`)
+    })
+}
+
 // === Middleware
-app.use(express.json({type: 'application/activity+json'}))
+app.use(express.json({ type: 'application/activity+json' }))
 
 // === Route
 app.get('/', (req, res) => {
@@ -20,19 +40,29 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/inbox', (req, res) => {
-  if (req.headers['content-type'] === 'application/activity+json') {
-    debug(`${req.method} ${req.path}`)
-    /** @type {{keyId: string}} */
-    const parsed = httpSignature.parseRequest(req, {});
-    const pub = readFileSync(new URL(parsed.keyId), 'ascii');
-    debug(httpSignature.verifySignature(parsed, pub))
-    debug(req.headers)
-    debug(req.body)
-  }
-})
+app.post('/inbox', validateRequest)
 
 // === Serve
 app.listen(port, () => {
   log.info(`${appName} app listening on port ${port}`)
 })
+
+/**
+ * 
+ * @param {express.Request} req 
+ */
+async function validateRequest(req) {
+  if (req.headers['content-type'] === 'application/activity+json') {
+    debug(`${req.method} ${req.path}`)
+    /** @type {{keyId: string}} */
+    const parsed = httpSignature.parseRequest(req, {})
+
+
+    debug(parsed.keyId)
+    const key = await fetchActorPublicKey(parsed.keyId)
+    const validHTTPSignature = httpSignature.verifySignature(parsed, key)
+    debug(`Signature is valid: ${validHTTPSignature}`)
+
+    debug(req.body)
+  }
+}
--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps : [{
     name: "toot-sweet",
     script: "./app.js",
-    watch: ['*.js'],
+    watch: ["*.js"],
     env: {
       NODE_ENV: "development",
       DEBUG: 'toot-sweet'
