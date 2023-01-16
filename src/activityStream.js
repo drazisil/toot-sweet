bccfb43adde9390daecbd3f6facb9c571cf0484f
Drazi Crendraven <190986+drazisil@users.noreply.github.com>
2023-01-16 12:44:09 -0500

    yay keys!

--- src/activityStream.js
+++ src/activityStream.js
@@ -1,3 +1,4 @@
+import { readFileSync } from 'node:fs';
 import { RequestWithBody } from '../app.js';
 import log from '../log.js';
 import { json404 } from './json404.js';
@@ -22,13 +23,22 @@ export class UsersController {
    * @param {RequestWithBody} requestWithBody 
    */
   respondUser(requestWithBody) {
+    const publicKeyPem = readFileSync('data/dev-key.pem', { encoding: "utf8" })
+
     /** @type {ActivityStreamObject | Record<string, string>} */
     const response = {
       "@context": ["https://www.w3.org/ns/activitystreams", "https://w3id.org/security/v1"],
       "id": this.baseUser,
       "type": "Person",
+      "name": "Drazi Test",
+      "preferredUsername": "drazi",
       "inbox": this.baseUser.concat("/inbox"),
       "outbox": this.baseUser.concat("/outbox"),
+      "publicKey": {
+        "id": this.baseUser.concat("#main-key"),
+        "owner": this.baseUser,
+        "publicKeyPem": publicKeyPem
+      }
     }
     requestWithBody.requestInfo.response.setHeader("content-type", 'application/activity+json')
 
