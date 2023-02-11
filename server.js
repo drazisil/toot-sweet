7e7e0fe5dedd3a5b5a4bf6a2638544dbf142b896
Drazi Crendraven <190986+drazisil@users.noreply.github.com>
2023-02-11 18:30:52 -0500

    Saving

--- server.js
+++ server.js
@@ -1,60 +1,57 @@
-import https from '@small-tech/https'
-import log from './lib/logger.js'
-import createExpress, * as express from "express"
-import wellKnownRouter from "./routes/wellknown.js";
-import peopleRouter from "./routes/people.js";
-import logRouter from "./routes/log.js"
-import groupsRouter from "./routes/groups.js"
-import createExpress from "express";
+import https from "@small-tech/https";
+import log from "./lib/logger.js";
+import createExpress, * as express from "express";
+import wellKnownRouter from "./lib/routes/wellknown.js";
+import peopleRouter from "./lib/routes/people.js";
+import logRouter from "./lib/routes/log.js";
+import groupsRouter from "./lib/routes/groups.js";
 import helmet from "helmet";
-import log from "./logger.js"
-import { Queue } from "./Queue.js";
-import { Grouper } from "./Grouper.js";
-import { Activity } from "./Activity.js";
+import { Queue } from "./lib/Queue.js";
+import { Grouper } from "./lib/Grouper.js";
+import { Activity } from "./lib/Activity.js";
 
-const app = createExpress()
+const app = createExpress();
 
-export const ROOT_DOMAIN = "mc.drazisil.com"
+export const ROOT_DOMAIN = "mc.drazisil.com";
 
 const options = {
   domains: [ROOT_DOMAIN],
-  settingsPath: "data"
-}
+  settingsPath: "data",
+};
 
-const grouper = Grouper.getGrouper()
+const grouper = Grouper.getGrouper();
 
-grouper.createGroup("activityStreamsInbound")
+grouper.createGroup("activityStreamsInbound");
 
-grouper.createGroup("actorsSeen")
+grouper.createGroup("actorsSeen");
 
-grouper.createGroup("remoteActors")
+grouper.createGroup("remoteActors");
 
-app.disable('x-powered-by')
+app.disable("x-powered-by");
 
-app.use(helmet())
+app.use(helmet());
 
-app.use(createExpress.json({ "type": "application/json" }))
+app.use(express.json({ type: "application/json" }));
 
-app.use(createExpress.json({ "type": "application/activity+json" }));
+app.use(express.json({ type: "application/activity+json" }));
 
 app.use((request, response, next) => {
   if (request.headers["content-type"]?.includes("application/activity+json")) {
-    const inboundActivity = Activity.fromRequest(request)
+    const inboundActivity = Activity.fromRequest(request);
 
     if (inboundActivity.type !== "Delete") {
-      grouper.addToGroup("activityStreamsInbound", inboundActivity)
+      grouper.addToGroup("activityStreamsInbound", inboundActivity);
     }
-    grouper.addToGroup("actorsSeen", inboundActivity.actor)
-
+    grouper.addToGroup("actorsSeen", inboundActivity.actor);
   }
-  next()
-})
+  next();
+});
 
 app.use("/log", logRouter);
 
 app.use("/groups", groupsRouter);
 
-app.use(requestLogger)
+app.use(requestLogger);
 
 app.use("/.well-known", wellKnownRouter);
 
@@ -66,26 +63,41 @@ app.use("/users", peopleRouter);
 app.use(createExpress.static("./public"));
 
 app.use((req, res, next) => {
-  const logLine = { "error": "not found", "method": req.method, "url": req.url }
+  const logLine = { error: "not found", method: req.method, url: req.url };
   // Queue.getQueue().add(logLine)
-  log.info(logLine)
-  next()
-})
+  log.info(logLine);
+  next();
+});
 
 try {
-
-  const server = https.createServer(options, app)
+  const server = https.createServer(options, app);
 
   server.listen(443, () => {
-    log.info(Object({ "server": { "status": "listening" } }))
-  })
+    log.info(Object({ server: { status: "listening" } }));
+  });
 
   server.on("error", (/** @type {unknown} */ err) => {
-    log.error(Object({ "server": { "status": "errored", "reason": String(err) } }))
-  })
+    log.error(Object({ server: { status: "errored", reason: String(err) } }));
+  });
 } catch (error) {
-  log.error({ "reason": `Fatal error!: ${{ error }}` })
-  process.exit(-1)
+  log.error({ reason: `Fatal error!: ${{ error }}` });
+  process.exit(-1);
 }
 
-
+/**
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ */
+function requestLogger(req, res, next) {
+  const logLine = {
+    headers: JSON.stringify(req.headers),
+    body: JSON.stringify(req.body),
+    method: req.method,
+    url: req.url,
+    remoteHost: req.socket.remoteAddress ?? "unknown",
+  };
+  Queue.getQueue().add({ timestamp: new Date().toISOString(), ...logLine });
+  next();
+}
