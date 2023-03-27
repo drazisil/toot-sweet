4eea2125f0e8ed212d8b13c48129f0f5a2fc7b7d
Drazi Crendraven <drazisil@hey.com>
2023-03-26 21:06:23 -0400

    Move Person to Models, test for PeopleConnector

---
+++ test/PeopleConnector.test.js
@@ -0,0 +1,26 @@
+import { PeopleConnector } from "../lib/PeopleConnector.js";
+import { describe, it } from "node:test";
+import { ok } from "node:assert/strict";
+
+describe("PeopleConnector", () => {
+    describe("getPeopleConnector", () => {
+        it("is a static method that returns an instance of PeopleConnector", () => {
+            // arrange
+            const expectedClass = PeopleConnector
+
+            /** @type {import("../lib/config.js").Env} */
+            const testConfig = {
+                siteHost: "",
+                blockList: [],
+                localHosts: [],
+                version: ""
+            }
+            
+            // act
+            const testPeopleConnector = PeopleConnector.getPeopleConnector(testConfig)
+
+            // assert
+            ok(testPeopleConnector instanceof expectedClass === true)
+        })
+    })
+})
